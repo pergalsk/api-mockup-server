0bbde91ee002c62ff885924b4551bc5c72a3787e
Peter Gál <pergalsk@gmail.com>
2021-05-08 21:49:24 +0200

    New: Better options support New: Better warning and errors Dev: Refactored code Dev: Added colored log service

--- index.js
+++ index.js
@@ -1,85 +1,126 @@
 const nodeFs = require('fs');
 const nodePath = require('path');
 const express = require('express');
-const cors = require('cors');
+const corsMiddleware = require('cors');
 const chalk = require('chalk');
-const utils = require('./lib/utils.js')
 
+const utils = require('./lib/utils');
+const log = require('./lib/log');
+const defaultOptions = require('./lib/definitions');
+const methods = require('./lib/methods');
 
-function simpleMockServer(options) {
+function smServer(options = {}) {
+  console.log(chalk.green('*** ') + `Starting Simple mock server.`);
+
+  // create express application
   const app = express();
 
-  // todo: allow to be as params
-  const port = options.port || 7766;
-  const routesPath = nodePath.join(process.cwd(), options.routesPath || './routes.js');
-  // const routes = require(options.routesPath || './routes.js');
-  const partials = options.databasePath || 'database';
-  const dataFilesEncoding = options.encoding || 'utf8';
-  const prefix = options.prefix || '/ibs';
-  const delayInterval = options.delayInterval || {
-    min: 150, // miliseconds
-    max: 1500 // miliseconds
+  const { port, routes, database, encoding, cors, prefix, delay: delayInterval } = {
+    ...defaultOptions.server,
+    ...options,
   };
 
-  if (options.cors === true) {
-    app.use(cors());
+  if (cors === true) {
+    app.use(corsMiddleware());
   }
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
   // load routes definitions in JS format
-  try {
-    if (nodeFs.existsSync(routesPath)) {
-      routes = require(routesPath);
-    }
-  } catch (e) {
-    console.log(chalk.red(`ERROR: File with routes definitions not found. Please define routes in "./routes.js" file.`));
-    return;
-  }
-
+  let routesList = [];
   if (Array.isArray(routes)) {
-    routes.forEach(registerRoute);
-  } else {
-    console.log(chalk.red(`WARNING: File with routes definitions has wrong format. It has to be array of routes definitions.`));
-    return;
+    routesList = [...routes];
+  } else if (typeof routes === 'string') {
+    try {
+      const routesPath = nodePath.join(process.cwd(), routes);
+      if (nodeFs.existsSync(routesPath) && nodeFs.lstatSync(routesPath).isFile()) {
+        routesList = require(routesPath);
+      }
+    } catch (e) {
+      routesList = [];
+    }
   }
 
+  if (routesList.length === 0) {
+    log.warning([
+      `WARNING: Missing routes definitions or definitions file has wrong format.`,
+      `Please define some routes in server options { routes: [ ... ] },`,
+      `or provide path to routes config JS file.`,
+      `For example in { routes: "./routes.js" } config file could contain:`,
+      `module.exports = [`,
+      `  { key: 'PRODUCT_CREATE', method: 'POST', path: '/api/product', status: '200' },`,
+      `  { key: 'PRODUCT_DETAIL', method: 'GET', path: '/api/product/:id', status: '200' },`,
+      `  { key: 'PRODUCT_DELETE', method: 'DELETE', path: '/api/product/:id', status: '201' }`,
+      `];`,
+      `Look into documentation for more details.`,
+    ]);
+  }
+
+  // generate rest API routes
+  routesList.forEach(registerRoute);
+
+  // starts server listening
   app.listen(port, () => {
-    console.log(chalk.red('***') + ` Mock server listening at http://localhost:${port}`);
-    console.log('Handled routes:');
+    console.log(chalk.green('*** ') + `Simple mock server listening at http://localhost:${port}`);
+    log.log('Handled routes:');
+    if (Array.isArray(routesList) && routesList.length === 0) {
+      log.info('No routes defined.');
+    }
   });
 
-
-  // mathods
+  // methods
 
   function registerRoute(route) {
-    const { key, method, path, status, delay, callback } = route;
-    let { data } = route
+    const { key, method, path, status, delay, callback } = {
+      ...defaultOptions.route,
+      ...route,
+    };
 
-    if (!key || !method || !path || !status) {
-      console.log(chalk.red('WARNING: No key, method, path or status found in routes list config item.'));
+    let { data } = route;
+
+    if (!path) {
+      log.error('ERROR: No path found in route config item.');
       return;
     }
 
     // todo: move to app.METHOD exec ?
-    if (!data) {
-      // data = require(partials + '/' + key + '.json');
-      // console.log(data);
+    if (data == null) {
+      if (key) {
+        const filePathDefault = nodePath.join(process.cwd(), database, key + '.json');
+        const filePathWithStatus = nodePath.join(
+          process.cwd(),
+          database,
+          key + '.' + status + '.json'
+        );
 
-      const filePathDefault = nodePath.join(process.cwd(), partials, key + '.json');
-      const filePathWithStatus = nodePath.join(process.cwd(), partials, key + '.' + status + '.json');
-
-      try {
-        // todo: make FS methods async ?
-        if (nodeFs.existsSync(filePathWithStatus)) {
-          data = nodeFs.readFileSync(filePathWithStatus, dataFilesEncoding);
-        } else {
-          data = nodeFs.readFileSync(filePathDefault, dataFilesEncoding);
+        try {
+          // todo: make FS methods async ?
+          if (nodeFs.existsSync(filePathWithStatus)) {
+            data = nodeFs.readFileSync(filePathWithStatus, encoding);
+          } else if (nodeFs.existsSync(filePathDefault)) {
+            data = nodeFs.readFileSync(filePathDefault, encoding);
+          } else {
+            log.warning([
+              `WARNING: Files with response data for key: ${key} not found:`,
+              `"${filePathWithStatus}"`,
+              `"${filePathDefault}"`,
+              `Route "${path}" will be served with empty response.`,
+            ]);
+          }
+        } catch (e) {
+          log.warning([
+            `WARNING: Files with response data for key: ${key} not found:`,
+            `"${filePathWithStatus}"`,
+            `"${filePathDefault}"`,
+            `Route "${path}" will be served with empty response.`,
+          ]);
         }
-      } catch (e) {
-        console.log(chalk.red(`WARNING: File with response data: "${filePathWithStatus}" or "${filePathDefault}" not found.`));
-        return;
+      } else {
+        log.warning([
+          `WARNING: You provided no data or key.`,
+          `Route "${path}" will be served with empty response.`,
+        ]);
       }
     }
 
@@ -87,10 +128,11 @@ function simpleMockServer(options) {
     const basePath = prefix + utils.getBasePath(path);
 
     // check for valid HTTP method
-    const methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'];
     if (methods.indexOf(method.toUpperCase()) === -1) {
-      console.log(chalk.red('WARNING: No valid method found in routes list config item.'));
-      console.log(chalk.red('WARNING: Available methods are: GET, POST, PUT, DELETE, HEAD, CONNECT, OPTIONS, TRACE, PATCH'));
+      log.warning([
+        'WARNING: No valid method found in routes list config item.',
+        'WARNING: Available methods are: ' + methods.join(', '),
+      ]);
       return;
     }
 
@@ -111,26 +153,23 @@ function simpleMockServer(options) {
 
       setTimeout(send, interval);
 
+      const isEmptyData = !data;
+
       // util function for response sending
       function send() {
         res.send(data);
-        utils.logRouteCall(req, method, status, key, utils.calculateHRTime(process.hrtime(startTime)));
+
+        utils.logRouteCall(
+          req,
+          method,
+          status,
+          key,
+          utils.calculateHRTime(process.hrtime(startTime)),
+          isEmptyData
+        );
       }
     });
   }
 }
 
-// EXAMPLE
-/*const smServer = require('simple-mock-server');
-smServer({
-  port: 9933,
-  routesPath: './paths',
-  databasePath: 'db',
-  encoding: 'utf8',
-  prefix: '/api',
-  delayInterval: {min: 10, max: 90},
-  cors: true,
-});*/
-
-
-module.exports = simpleMockServer;
+module.exports = smServer;
